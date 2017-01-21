72a7aaab1273fd9160b348da86d3d501270139dd
Marlena Abraham <Marlena Abraham>
2017-01-21 12:58:16 -0500

    Added an ammo manager

--- UnityProjects/GGJ17/Assets/Code/AmmoManager.cs
+++ UnityProjects/GGJ17/Assets/Code/AmmoManager.cs
@@ -13,8 +13,9 @@ public class AmmoManager : MonoBehaviour {
     public int initialGuesses = 3;
 
     [Header("Current Ammo Levels")]
-    public int currentBoopsRemaining;
-    public int currentGuessesRemaining;
+    public int boopsRemaining;
+    public int guessesRemaining;
+    private List<GameObject> guessUIList = new List<GameObject>();
 
     private void Start()
     {
@@ -22,10 +23,31 @@ public class AmmoManager : MonoBehaviour {
         for(int i = 0; i < initialGuesses; i++)
         {
             GameObject nextIcon = Instantiate(guessUIPrefab) as GameObject;
+            guessUIList.Add(nextIcon);
             nextIcon.transform.SetParent(guessUIParent.transform, false);
 
             float newX = transform.parent.localPosition.x + (guessXOffset + guessPrefabWidth)* i;
             nextIcon.transform.localPosition = new Vector3(newX, 0, 0);
+        }
+
+        boopsRemaining = initialBoops;
+        guessesRemaining = initialGuesses;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("Fire!");
+            GameObject removingThisUI = guessUIList[guessUIList.Count - 1];
+            guessUIList.Remove(removingThisUI);
+            Destroy(removingThisUI);
+
+            guessesRemaining -= 1;
+            if (guessesRemaining == 0)
+            {
+                Debug.Log("Game over");
+            }
         }
     }
 
