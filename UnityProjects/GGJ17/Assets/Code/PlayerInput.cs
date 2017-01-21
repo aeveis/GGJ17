2fce795412d96698a92156228fa8708e421153a4
Marlena Abraham <Marlena Abraham>
2017-01-21 10:58:16 -0500

    Clicking makes the waves go.

--- UnityProjects/GGJ17/Assets/Code/PlayerInput.cs
+++ UnityProjects/GGJ17/Assets/Code/PlayerInput.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInput : MonoBehaviour
 {
+    public UnityEvent onBooped;
+
     private void OnMouseDown()
     {
+        onBooped.Invoke();
         Debug.Log("Boop!");
     }
 }
