213a53f96d64036c99a3717f0946d56690b95a97
Andrew Boyer <boyer.andrew.7@gmail.com>
2017-01-21 12:23:06 -0500

    boidField beta

---
+++ UnityProjects/GGJ17/Assets/Code/BoidField.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidField : MonoBehaviour {
+
+	//gameObject Boid;
+	public GameObject boidPrototype;
+
+	// attributes of field boids
+	public float ScreenWidth = 16f;
+	public float ScreenHeight = 10f;
+	float distanceBetweenBoids = .2f;
+	float totalBoids = 100;
+
+	// Use this for initialization
+	void Start () {
+
+
+		for (var i = - ScreenWidth/2; i < ScreenWidth/2; i += distanceBetweenBoids) 
+		{
+			for (var ii = - ScreenHeight/2; ii < ScreenHeight / 2; ii += distanceBetweenBoids) 
+			{
+				GameObject go = Instantiate (boidPrototype);
+				go.transform.SetParent (transform);
+				go.transform.position = new Vector3 (i, ii, 0);
+			}
+		}
+
+
+	}
+
+}
