9a581d1b2a10ca32e0d4f39becfaed288a36bc2d
Marlena Abraham <Marlena Abraham>
2017-01-21 11:58:28 -0500

    Added an ammo manager dummy

---
+++ UnityProjects/GGJ17/Assets/Code/AmmoManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour {
+    [Header("Initial Ammo")]
+    public int initialBoops = 5;
+    public int initialGuesses = 3;
+
+    [Header("Current Ammo Levels")]
+    public int currentBoopsRemaining;
+    public int currentGuessesRemaining;
+
+     
+}
