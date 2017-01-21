298cdf8de001ffb0f71cb153e5df3b3e6acad6e1
Marlena Abraham <Marlena Abraham>
2017-01-21 12:42:50 -0500

    Added a guesses UI

--- UnityProjects/GGJ17/Assets/Code/AmmoManager.cs
+++ UnityProjects/GGJ17/Assets/Code/AmmoManager.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoManager : MonoBehaviour {
+    [Header("UI Prefabs")]
+    public GameObject guessUIPrefab;
+    public GameObject guessUIParent;
+    public float guessXOffset = 10f;
+
     [Header("Initial Ammo")]
     public int initialBoops = 5;
     public int initialGuesses = 3;
@@ -11,5 +16,17 @@ public class AmmoManager : MonoBehaviour {
     public int currentBoopsRemaining;
     public int currentGuessesRemaining;
 
-     
+    private void Start()
+    {
+        float guessPrefabWidth = guessUIPrefab.GetComponent<RectTransform>().rect.width;
+        for(int i = 0; i < initialGuesses; i++)
+        {
+            GameObject nextIcon = Instantiate(guessUIPrefab) as GameObject;
+            nextIcon.transform.SetParent(guessUIParent.transform, false);
+
+            float newX = transform.parent.localPosition.x + (guessXOffset + guessPrefabWidth)* i;
+            nextIcon.transform.localPosition = new Vector3(newX, 0, 0);
+        }
+    }
+
 }
