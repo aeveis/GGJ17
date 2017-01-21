f31419684521718a3e2b77f5d44d0648ca6afb5a
Miyth <forgottenmiyth@gmail.com>
2017-01-21 16:52:00 -0500

    Adding a boid finder.

---
+++ UnityProjects/GGJ17/Assets/BoidFinder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BoidFinder : MonoBehaviour 
+{
+    List<Boid> nearbyBoids = new List<Boid>();
+
+    public Boid GetClosestBoid()
+    {
+        
+        Boid bestBoid = null;
+        float bestDistance = -1f;
+
+        if (nearbyBoids.Count > 0)
+        {
+            bestBoid = nearbyBoids[0];
+            if (nearbyBoids.Count == 1)
+                return bestBoid;
+            
+            bestDistance = Vector3.Distance(transform.position, nearbyBoids[0].transform.position);
+        }
+        else
+            return null;
+        
+        for (int i = 1; i < nearbyBoids.Count; i++)
+        {
+            float testDistance = Vector3.Distance(transform.position, nearbyBoids[i].transform.position);
+            if (testDistance < bestDistance)
+            {
+                bestDistance = testDistance;
+                bestBoid = nearbyBoids[i];
+            }
+        }
+        return bestBoid;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Boid newNeighbor = col.GetComponent<Boid>();
+
+        if (newNeighbor && nearbyBoids.Contains(newNeighbor) == false)
+            nearbyBoids.Add(newNeighbor);
+    }
+}
