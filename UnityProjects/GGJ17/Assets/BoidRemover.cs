862ceaa31c3f25ec76a7fab1214fe848d4dad75f
Marlena Abraham <Marlena Abraham>
2017-01-21 16:00:23 -0500

    Added boid things

---
+++ UnityProjects/GGJ17/Assets/BoidRemover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidRemover : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.SetActive(true);
+    }
+}
