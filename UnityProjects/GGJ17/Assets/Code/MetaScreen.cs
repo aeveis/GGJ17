cae0c298cecd6409eff53e6b41455a646222db41
Marlena Abraham <Marlena Abraham>
2017-01-21 18:35:36 -0500

    Added level loading architecture

--- UnityProjects/GGJ17/Assets/Code/MetaScreen.cs
+++ UnityProjects/GGJ17/Assets/Code/MetaScreen.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MetaScreen : MonoBehaviour {
     [Header("Level Info")]
     public int currentLevel;
     public int lastUnlockedLevel;
     public bool currentLevelComplete = false;
+    public List<string> allLevels;
 
     [Header("Pause Handler")]
     public GameObject PauseScreen;
@@ -15,6 +17,19 @@ public class MetaScreen : MonoBehaviour {
     private void Start()
     {
         PauseScreen.SetActive(false);
+        LoadSceneNumber(1);
+    }
+
+    private void LoadSceneNumber(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber, mode: LoadSceneMode.Additive);
+    }
+
+    private void NextScene()
+    {
+        currentLevel++;
+        SceneManager.UnloadSceneAsync(allLevels[currentLevel-1]);
+        SceneManager.LoadScene(allLevels[currentLevel], mode: LoadSceneMode.Additive);
     }
 
     private void Update()
@@ -24,6 +39,11 @@ public class MetaScreen : MonoBehaviour {
             isInMenuOverlay = !isInMenuOverlay;
             if (isInMenuOverlay) { PauseScreen.SetActive(true); }
             else { PauseScreen.SetActive(false); }
+        }
+
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            NextScene();
         }
     }
 }
