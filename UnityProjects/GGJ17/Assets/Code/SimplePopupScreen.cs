7f8024a483e0317b8e0ab26a7e2da7db2ddb9b19
Miyth <forgottenmiyth@gmail.com>
2017-01-22 02:39:32 -0500

    More overhauls to Game Manager.

---
+++ UnityProjects/GGJ17/Assets/Code/SimplePopupScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimplePopupScreen : MonoBehaviour 
+{
+    public Animator animator;
+
+    public void SetShow(bool show)
+    {
+        animator.SetBool("Show", show);
+    }
+}
